a9af368f1596bf9d6958647b9175041ce5abce52
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-18 10:02:23 +0900

    Link名前付きルート

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  テストです
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -5,5 +5,7 @@ import { Link } from '@inertiajs/inertia-vue3';
 <template>
   Inertiaテストです<br />
   <a href="/">aタグでWelcomeに移動</a><br />
-  <Link href="/">LinkでWelcomeに移動</Link>
+  <Link href="/">LinkでWelcomeに移動</Link><br>
+  <Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
+
 </template>
