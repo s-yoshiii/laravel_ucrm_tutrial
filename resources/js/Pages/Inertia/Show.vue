60d3c2b8e18a3d1dcef38fdb9d038a76773c8104
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-20 00:00:48 +0900

    Linkルート付きパラメータ

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineConfig } from 'rollup';
+
+ defineProps({
+  id:String
+ })
+</script>
+
+
+<template>
+  {{ id }}
+</template>
+
+
+<style>
+
+</style>
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -7,5 +7,6 @@ import { Link } from '@inertiajs/inertia-vue3';
   <a href="/">aタグでWelcomeに移動</a><br />
   <Link href="/">LinkでWelcomeに移動</Link><br>
   <Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
+  <Link :href="route('inertia.show',{id : 50})">ルートパラメータの確認です</Link><br>
 
 </template>
