d951f4fbed202b18c49506d47d910e97145302ee
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-24 23:23:00 +0900

    defineEmits

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,15 @@
 import GuestLayoutVue from "@/Layouts/GuestLayout.vue";
 import InputLabelVue from "@/Components/InputLabel.vue";
 import TextInputVue from "@/Components/TextInput.vue";
+const emitTest = (e) => console.log(e);
 </script>
 
 <template>
     <GuestLayoutVue>
         <InputLabelVue value="件名">タイトル</InputLabelVue>
-        <TextInputVue modelValue="初期値が入ります" />
+        <TextInputVue
+            modelValue="初期値が入ります"
+            @update:modelValue="emitTest"
+        />
     </GuestLayoutVue>
 </template>
