e9022a15a6244e184b89841e83fc03410670a59a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-28 00:56:30 +0900

    ItemSeeder

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,6 +37,12 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
+                                <NavLink
+                                    :href="route('items.index')"
+                                    :active="route().current('items.index')"
+                                >
+                                    商品管理
+                                </NavLink>
                             </div>
                         </div>
 
